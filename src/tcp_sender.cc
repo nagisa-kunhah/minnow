3327d287a2464c156581d795ae684604e26570b5
nagisa <1434936049@qq.com>
2024-03-19 13:23:29 +0800

    pass checkpoint 3

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,103 +1,170 @@
 #include "tcp_sender.hh"
+
 #include "tcp_config.hh"
 
 using namespace std;
 
-uint64_t TCPSender::sequence_numbers_in_flight() const
-{
-  // Your code here.
-  return lastSeq_-lastACK_;
+uint64_t TCPSender::sequence_numbers_in_flight() const {
+  return bytesInFlight_;
 }
 
-uint64_t TCPSender::consecutive_retransmissions() const
-{
-  // Your code here.
-  return consecutiveRetryNum_;
+uint64_t TCPSender::consecutive_retransmissions() const {
+  return consecutiveRetransmissions_;
 }
 
-void TCPSender::push( const TransmitFunction& transmit )
-{
-  // Your code here.
+void TCPSender::push(const TransmitFunction& transmit) {
+  cerr << "to push..." << endl;
+  cerr << "now:" << input_.reader().peek() << endl;
   (void)transmit;
-  uint64_t nowWindows=windowsSizeHaveSet?windowsSize_:1;
-  while(true){
-    auto msg=TCPSenderMessage();
-    if(!hasSyn_){
-      msg.seqno=Wrap32::wrap(0,isn_);
-      msg.SYN=true;
-      msg.RST=input_.reader().has_error();
-      nowWindows-=1;
-    }else{
-      uint64_t nowLen=min({TCPConfig::MAX_PAYLOAD_SIZE,nowWindows,input_.reader().bytes_buffered()});
-      string payload{input_.reader().peek().substr(0,nowLen)};
-      input_.reader().pop(nowLen);
-      msg.seqno=Wrap32::wrap(lastSeq_,isn_);
-      msg.payload=payload;
-      msg.RST=input_.reader().has_error();
-      nowWindows-=msg.sequence_length();
+  if (!haveSyn_) {
+    haveSyn_ = true;
+    TCPSenderMessage msg;
+    msg.SYN = true;
+    msg.RST = input_.has_error();
+    msg.seqno = Wrap32::wrap(Seqno_, isn_);
+    if (input_.reader().is_finished() && !haveFin_ &&
+        receiverWindows_ > Seqno_ - lastAck_) {
+      msg.FIN = true;
+      haveFin_ = true;
     }
-    if(input_.writer().is_closed()&&!hasFin_&&msg.sequence_length()+1<=windowsSize_){
-      msg.FIN=true;
-      hasFin_=true;
+    Seqno_ += msg.sequence_length();
+    bytesInFlight_ += msg.sequence_length();
+    outstandingSegments_.push_back(msg);
+    transmit(msg);
+    timerWork_ = !outstandingSegments_.empty();
+    cerr << "after push 1" << endl;
+    for (auto x : outstandingSegments_) {
+      cerr << x.seqno.raw_value_ << " " << x.SYN << " " << x.FIN << " "
+           << x.payload << endl;
     }
-    lastSeq_+=msg.sequence_length();
-    if(msg.sequence_length()!=0){
-      transmit(msg);
-      data_.push_back(msg);
-    }
-    if(nowWindows==0||input_.reader().is_finished()||input_.reader().bytes_buffered()==0){
-      break;
-    }
+    cerr << endl;
+    return;
   }
-  windowsSize_=nowWindows;
+  uint64_t nowWindows = receiverWindows_ > 0 ? receiverWindows_ : 1;
+  if (input_.reader().is_finished() && !haveFin_ &&
+      nowWindows > Seqno_ - lastAck_) {
+    cerr << "push 2 con:" << endl;
+    cerr << input_.reader().is_finished() << " " << (!haveFin_) << " "
+         << receiverWindows_ << " " << Seqno_ - lastAck_ << endl;
+    haveFin_ = true;
+    TCPSenderMessage msg;
+    msg.FIN = true;
+    msg.RST = input_.has_error();
+    msg.seqno = Wrap32::wrap(Seqno_, isn_);
+    Seqno_ += msg.sequence_length();
+    bytesInFlight_ += msg.sequence_length();
+    outstandingSegments_.push_back(msg);
+    transmit(msg);
+    timerWork_ = !outstandingSegments_.empty();
+    cerr << "after push 2" << endl;
+    for (auto x : outstandingSegments_) {
+      cerr << x.seqno.raw_value_ << " " << x.SYN << " " << x.FIN << " "
+           << x.payload << endl;
+    }
+    cerr << endl;
+    return;
+  }
+  while (input_.reader().bytes_buffered() != 0 &&
+         lastAck_ + nowWindows > Seqno_) {
+    TCPSenderMessage msg;
+    uint64_t nowLen =
+        min({input_.reader().bytes_buffered(), nowWindows - (Seqno_ - lastAck_),
+             TCPConfig::MAX_PAYLOAD_SIZE});
+    string payload{input_.reader().peek().substr(0, nowLen)};
+    input_.reader().pop(nowLen);
+    msg.seqno = Wrap32::wrap(Seqno_, isn_);
+    msg.payload = payload;
+    msg.RST = input_.reader().has_error();
+    if (input_.reader().is_finished() && !haveFin_ &&
+        bytesInFlight_ + msg.sequence_length() < nowWindows) {
+      msg.FIN = true;
+      haveFin_ = true;
+      msg.RST = input_.has_error();
+    }
+    Seqno_ += msg.sequence_length();
+    bytesInFlight_ += msg.sequence_length();
+    outstandingSegments_.push_back(msg);
+    transmit(msg);
+  }
+  timerWork_ = !outstandingSegments_.empty();
+  cerr << "after push 3" << endl;
+  for (auto x : outstandingSegments_) {
+    cerr << x.seqno.raw_value_ << " " << x.SYN << " " << x.FIN << " "
+         << x.payload << endl;
+  }
+  cerr << endl;
 }
 
-TCPSenderMessage TCPSender::make_empty_message() const
-{
+TCPSenderMessage TCPSender::make_empty_message() const {
   // Your code here.
   TCPSenderMessage msg;
-  msg.seqno=Wrap32::wrap(lastSeq_,isn_);
-  msg.RST=input_.reader().has_error();
+  msg.seqno = Wrap32::wrap(Seqno_, isn_);
+  msg.RST = input_.has_error();
   return msg;
 }
 
-void TCPSender::receive( const TCPReceiverMessage& msg )
-{
+void TCPSender::receive(const TCPReceiverMessage& msg) {
   // Your code here.
   (void)msg;
-  if(msg.ackno.has_value()){
-    uint64_t ackNo=msg.ackno.value().unwrap(isn_,0);
-    if(ackNo>lastSeq_){
-      return ;
-    }
-    lastACK_=max(lastACK_,msg.ackno.value().unwrap(isn_,0));
-    while(!data_.empty()&&data_.front().seqno.unwrap(isn_,0)+data_.front().sequence_length()-1<=ackNo){
-      if(data_.front().SYN){
-        hasSyn_=true;
-      }
-      data_.pop_front();
-      retransmissionTimer_=0;
-    }
+  if (msg.RST) {
+    input_.writer().set_error();
   }
-  consecutiveRetryNum_=0;
-  windowsSize_=msg.window_size;
-  windowsSizeHaveSet=true;
+  cerr << "receive:"
+       << (msg.ackno.has_value() ? msg.ackno.value().raw_value_ : -1) << " "
+       << msg.window_size << endl;
+  if (msg.ackno.has_value() && msg.ackno.value().unwrap(isn_, 0) > Seqno_) {
+    cerr << "receive: ignore" << endl;
+    return;
+  }
+  if (msg.ackno.has_value()) {
+    uint64_t ackNo = msg.ackno.value().unwrap(isn_, 0);
+    lastAck_ = ackNo;
+    receiverWindows_ = msg.window_size;
+    while (!outstandingSegments_.empty() &&
+           outstandingSegments_.front().seqno.unwrap(isn_, 0) +
+                   outstandingSegments_.front().sequence_length() <=
+               lastAck_) {
+      accumulatedTime_ = 0;
+      bytesInFlight_ -= outstandingSegments_.front().sequence_length();
+      consecutiveRetransmissions_ = 0;
+      outstandingSegments_.pop_front();
+    }
+    cerr << "after receive:" << ackNo << ", data:" << endl;
+    for (auto x : outstandingSegments_) {
+      cerr << x.seqno.unwrap(isn_, 0) << " " << x.payload << " " << x.SYN << " "
+           << x.FIN << endl;
+    }
+    cerr << "bytes in flight:" << bytesInFlight_ << endl;
+  }
+  receiverWindows_ = msg.window_size;
+  timerWork_ = !outstandingSegments_.empty();
+  cerr << "receive exit" << endl;
 }
 
-void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
-{
+void TCPSender::tick(uint64_t ms_since_last_tick,
+                     const TransmitFunction& transmit) {
   // Your code here.
   (void)ms_since_last_tick;
   (void)transmit;
   (void)initial_RTO_ms_;
-  retransmissionTimer_+=ms_since_last_tick;
-  if(retransmissionTimer_>=(initial_RTO_ms_<<consecutiveRetryNum_)){
-    if(!data_.empty()){
-      if(!windowsSizeHaveSet||windowsSize_!=0){
-        consecutiveRetryNum_+=1;
-      }
-      transmit(data_.front());
+  cerr << "tick:" << ms_since_last_tick << endl;
+  if (!timerWork_) {
+    cerr << "tick:"
+         << "not work,exit" << endl;
+    return;
+  }
+  accumulatedTime_ += ms_since_last_tick;
+  cerr << "now accumulated:" << accumulatedTime_ << endl;
+  cerr << "timeout need:" << (initial_RTO_ms_ << consecutiveRetransmissions_)
+       << endl;
+  if (accumulatedTime_ >= (initial_RTO_ms_ << consecutiveRetransmissions_)) {
+    cerr << "timeout! Retransmit" << endl;
+    transmit(outstandingSegments_.front());
+    accumulatedTime_ = 0;
+    cerr << "condition:" << receiverWindows_ << " " << haveSyn_ << endl;
+    if (receiverWindows_ != 0 || outstandingSegments_.front().SYN) {
+      cerr << "consecutiveRetransmissions_ up" << endl;
+      consecutiveRetransmissions_ += 1;
     }
-    retransmissionTimer_=0;
   }
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -1,9 +1,5 @@
 #pragma once
 
-#include "byte_stream.hh"
-#include "tcp_receiver_message.hh"
-#include "tcp_sender_message.hh"
-
 #include <cstdint>
 #include <functional>
 #include <list>
@@ -11,50 +7,58 @@
 #include <optional>
 #include <queue>
 
-class TCPSender
-{
-public:
-  /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
-  TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
-  {}
+#include "byte_stream.hh"
+#include "tcp_receiver_message.hh"
+#include "tcp_sender_message.hh"
+
+class TCPSender {
+ public:
+  /* Construct TCP sender with given default Retransmission Timeout and possible
+   * ISN */
+  TCPSender(ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms)
+      : input_(std::move(input)), isn_(isn), initial_RTO_ms_(initial_RTO_ms) {}
 
   /* Generate an empty TCPSenderMessage */
   TCPSenderMessage make_empty_message() const;
 
   /* Receive and process a TCPReceiverMessage from the peer's receiver */
-  void receive( const TCPReceiverMessage& msg );
+  void receive(const TCPReceiverMessage& msg);
 
-  /* Type of the `transmit` function that the push and tick methods can use to send messages */
-  using TransmitFunction = std::function<void( const TCPSenderMessage& )>;
+  /* Type of the `transmit` function that the push and tick methods can use to
+   * send messages */
+  using TransmitFunction = std::function<void(const TCPSenderMessage&)>;
 
   /* Push bytes from the outbound stream */
-  void push( const TransmitFunction& transmit );
+  void push(const TransmitFunction& transmit);
 
-  /* Time has passed by the given # of milliseconds since the last time the tick() method was called */
-  void tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit );
+  /* Time has passed by the given # of milliseconds since the last time the
+   * tick() method was called */
+  void tick(uint64_t ms_since_last_tick, const TransmitFunction& transmit);
 
   // Accessors
-  uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
-  uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
+  uint64_t sequence_numbers_in_flight()
+      const;  // How many sequence numbers are outstanding?
+  uint64_t consecutive_retransmissions()
+      const;  // How many consecutive *re*transmissions have happened?
   Writer& writer() { return input_.writer(); }
   const Writer& writer() const { return input_.writer(); }
 
   // Access input stream reader, but const-only (can't read from outside)
   const Reader& reader() const { return input_.reader(); }
 
-private:
+ private:
   // Variables initialized in constructor
   ByteStream input_;
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
-  size_t windowsSize_{0};
-  bool windowsSizeHaveSet{false};
-  bool hasSyn_{false};
-  bool hasFin_{false};
-  uint64_t lastSeq_{0};
-  uint64_t lastACK_{0};
-  uint64_t consecutiveRetryNum_{0};
-  uint64_t retransmissionTimer_{0};
-  std::deque<TCPSenderMessage>data_{};
+  bool timerWork_{false};
+  bool haveSyn_{false};
+  bool haveFin_{false};
+  uint64_t accumulatedTime_{0};
+  uint64_t Seqno_{0};
+  uint64_t lastAck_{0};
+  uint64_t receiverWindows_{0};
+  uint64_t bytesInFlight_{0};
+  uint64_t consecutiveRetransmissions_{0};
+  std::deque<TCPSenderMessage> outstandingSegments_{};
 };
